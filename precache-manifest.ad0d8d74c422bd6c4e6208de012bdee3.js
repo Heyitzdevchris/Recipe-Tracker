7cd7c3d7baa9039746ca32bcacff1342d3859104
Chris <chrisaaron184@gmail.com>
2023-05-23 16:56:06 -0700

    Updates

---
+++ precache-manifest.ad0d8d74c422bd6c4e6208de012bdee3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bfa16c6eae0c3ce1b2a61c8faff4c7b",
+    "url": "/Recipe-Tracker/index.html"
+  },
+  {
+    "revision": "7a85c5ac8ab4a2fbab6a",
+    "url": "/Recipe-Tracker/static/css/main.53c92fb2.chunk.css"
+  },
+  {
+    "revision": "79a90c2036da4e83c020",
+    "url": "/Recipe-Tracker/static/js/2.6285a6b8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Recipe-Tracker/static/js/2.6285a6b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a85c5ac8ab4a2fbab6a",
+    "url": "/Recipe-Tracker/static/js/main.e1c6c6cd.chunk.js"
+  },
+  {
+    "revision": "c148e04949ec7a8342e0",
+    "url": "/Recipe-Tracker/static/js/runtime-main.bbe5f1c0.js"
+  }
+]);
